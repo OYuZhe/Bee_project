54b3f377a3333ba57978ba5b95c7f2a0bf9b8d45
1053021 <e96031001@gmail.com>
2019-05-19 12:27:47 +0800

    update write Log

--- svm.py
+++ svm.py
@@ -1,13 +1,20 @@
 import os
 import numpy as np
 import pandas as pd 
-
+import time
 from sklearn import metrics
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 
-def svm(Feature,Label):
+def WriteLog(accuracy,error):
+	now = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+	LogName = "svm.log-"+now
+	with open(LogName,'w') as f: 
+		f.write("Accuracy:"+str(accuracy)+'\n')
+		f.write("Error:"+str(error))
 
+def svm(Feature,Label):
+	print("start do svm")
 	#separate Feature and Label for train and test
 	X_train, X_test, Y_train, Y_test =train_test_split(Feature, Label, test_size = 0.3)
 
@@ -15,23 +22,24 @@ def svm(Feature,Label):
 	Svm_Model = SVC(gamma='scale',verbose=False,decision_function_shape='ovo')
 	#train model
 	Svm_Model.fit(X_train, Y_train)
-
+	print("Finish")
 	#compute Accuracy
 	predicted = Svm_Model.predict(X_test)
 	accuracy = metrics.accuracy_score(Y_test, predicted)
-	print(accuracy)
+	print("Accuracy:",accuracy)
 
 	#compute how many predict is false
 	error = 0
 	for i  in range(0,len(Y_test)):
 		if predicted[i] != Y_test[i]:
 			error+=1
-	print(error)
-	
+	print("Error:",error)
+	WriteLog(accuracy,error)
 	
 if __name__ == '__main__':
 	#read csv
-	df = pd.read_csv('Feature.csv')
+	print("read csv...")
+	df = pd.read_csv('Feature1.csv')
 	#transform DataFrame to numpy.array
 	DataSet=df.values 
 	#print(type(DataSet))
@@ -39,5 +47,4 @@ if __name__ == '__main__':
 	Feature = DataSet[:,2:]
 	Label = DataSet[:,1:2]
 	Label = Label.flatten()
-
 	svm(Feature,Label)
